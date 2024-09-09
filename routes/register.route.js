824283bd3f75ff6d61412a0356489135c0ecf0df
djancool016 <dwijulianto16@gmail.com>
2024-09-09 23:33:18 +0700

    base crud routes

---
+++ routes/register.route.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+const config = require('../config')
+const {controller: {sendResponse}} = require('dwij-simple-orm').init(config)
+const controller = require('../controllers/register.controller')
+
+// create
+router.post('/', controller.create, sendResponse)
+
+// find by params id
+router.get('/:id', controller.read, sendResponse) 
+
+// find by request body
+router.get('/', controller.read, sendResponse) 
+
+// update
+router.put('/', controller.update, sendResponse) 
+
+// delete by params id
+router.delete('/:id', controller.destroy, sendResponse) 
+
+module.exports = router
