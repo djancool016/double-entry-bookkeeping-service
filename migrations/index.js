6db8c20738838071d32efc52aa5ae8c286cbb4f8
djancool016 <dwijulianto16@gmail.com>
2024-09-12 11:57:34 +0700

    refactor

--- migrations/index.js
+++ migrations/index.js
@@ -1,13 +1,6 @@
 const config = require('../config')
 const {poolManager, runMigrations} = require('dwij-simple-orm').init(config)
-
-const migrationsObject = {
-    account: require('./202407201449-create-account'),
-    coa: require('./202407201450-create-coa'),
-    register: require('./202407201451-create-register'),
-    entry: require('./202407201455-create-entry'),
-    transaction: require('./202407221351-create-transaction'),
-}
+const migrationsObject = require('./migrations')
 
 const migrations = Object.values(migrationsObject)
 const pool = poolManager.connect()
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router()
 
 // add route module
+router.use('/account', require('./account.route'))
+router.use('/coa', require('./coa.route'))
+router.use('/entry', require('./entry.route'))
+router.use('/register', require('./register.route'))
+router.use('/transaction', require('./transaction.route'))
 
 module.exports = (app) => {
     app.use('/api', router)
